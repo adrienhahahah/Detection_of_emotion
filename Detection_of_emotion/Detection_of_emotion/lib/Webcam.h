d1d6c87d4d8a803c4bb618776be95af55ec455d9
adrienhahahah <adrienhahahah@outlook.com>
2019-11-09 14:06:34 +0100

    UTBM/IN52 course project, detection of emotions, first commit, facial traits detection works fine.

---
+++ Detection_of_emotion/Detection_of_emotion/lib/Webcam.h
@@ -0,0 +1,25 @@
+/*
+ * Webcam.h
+ *
+ *  Created on: 20 sept. 2010
+ *      Author: matthieu
+ */
+
+#ifndef WEBCAM_H_
+#define WEBCAM_H_
+
+#include "cv.h"
+#include "highgui.h"
+
+using namespace cv;
+
+class Webcam {
+public:
+	Webcam();
+	virtual ~Webcam();
+	void getImage(Mat& image);
+protected:
+	CvCapture* capture;
+};
+
+#endif /* WEBCAM_H_ */
